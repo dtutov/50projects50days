5f9919cf3dc08ea1954980685be8acc7e6713432
Denis Tutov <vrednokot@gmail.com>
2022-04-12 20:28:14 +0300

    feat: adds drag and drop

--- Day 4 - Drag N Drop/script.js
+++ Day 4 - Drag N Drop/script.js
@@ -1,25 +1,39 @@
-const fillBoxes = document.querySelectorAll('.fill');
+const fillBox = document.querySelector('.fill');
 const emptyBoxes = document.querySelectorAll('.empty-box');
 
-for(const empty of emptyBoxes) {
-	empty.addEventListener('dragover', dragStart);
-	empty.addEventListener('dragenter', dragOver);
-	empty.addEventListener('dragleave', dragEnter);
-	empty.addEventListener('drop', dragLeave);
+fillBox.addEventListener('dragstart', dragStart);
+fillBox.addEventListener('dragend', dragEnd);
+
+emptyBoxes.forEach((empty) => {
+	empty.addEventListener('dragover', dragOver);
+	empty.addEventListener('dragenter', dragEnter);
+	empty.addEventListener('dragleave', dragLeave);
+	empty.addEventListener('drop', dragDrop);
+});
+
+function dragStart(e) {
+	this.className += ' hold';
+	setTimeout(() => this.className = 'invisible', 0);
+};
+
+function dragEnd() {
+	this.className = 'fill';
+};
+
+function dragOver(evt) {
+	evt.preventDefault();
+};
+
+function dragEnter(evt) {
+	evt.preventDefault();
+	this.className += ' hovered';
+};
+
+function dragLeave() {
+	this.className = 'empty-box';
+};
+
+function dragDrop() {
+	this.className = 'empty-box';
+	this.append(fillBox);
 }
-
-function dragStart () {
-	console.log('start');
-};
-
-function dragOver () {
-	console.log('over');
-};
-
-function dragEnter () {
-	console.log('enter');
-};
-
-function dragLeave () {
-	console.log('leave');
-};
