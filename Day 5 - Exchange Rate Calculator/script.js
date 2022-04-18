f08894ef1aaddf86f3c11babe65b32523f0b8f98
Denis Tutov <vrednokot@gmail.com>
2022-04-18 20:10:41 +0300

    feat: adds markup for day5 project

---
+++ Day 5 - Exchange Rate Calculator/script.js
@@ -0,0 +1,7 @@
+const API_KEY = '4bff45a009000f23fa5d1e99';
+
+function calculate(currencyBase, currencyTarget) {
+	// fetch(`https://v6.exchangerate-api.com/v6/${API_KEY}/latest/${currencyBase}`)
+	// 	.then(res => res.json())
+	// 	.then(data => console.log(data))
+};
